883815e85d8100784b365b0f0a8852184c2c398f
yashshavikashyap@gmail.com <yashshavikashyap@gmail.com>
2021-10-31 13:00:35 +0000

    corrected syntax

--- models/covid19_models/location_population_data_model.sql
+++ models/covid19_models/location_population_data_model.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='table') }}
-SELECT DATE(DATE), LOCATION, Population, Population_Density AS Population_Density
+SELECT DATE(DATE), LOCATION, Population, Population_Density
 FROM {{source("covid19_data", "ALL_DATA")}}
