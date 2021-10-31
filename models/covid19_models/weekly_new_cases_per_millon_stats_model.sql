8eb21a8ba8dd8ef3060264a39dca9465019ef07f
yashshavikashyap@gmail.com <yashshavikashyap@gmail.com>
2021-10-31 13:11:12 +0000

    Updated column name

--- models/covid19_models/weekly_new_cases_per_millon_stats_model.sql
+++ models/covid19_models/weekly_new_cases_per_millon_stats_model.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT DATE(DATE), LOCATION, New_Cases_per_Million AS New_Cases_per_Million, 
+SELECT DATE(DATE) AS DATE, LOCATION, New_Cases_per_Million AS New_Cases_per_Million, 
 Total_Cases_per_Million AS Total_Cases_per_Million
 FROM {{source("covid19_data", "ALL_DATA")}}
--- models/covid19_models/weekly_new_deaths_stats_model.sql
+++ models/covid19_models/weekly_new_deaths_stats_model.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 
-SELECT DATE(DATE), LOCATION, 
+SELECT DATE(DATE) AS DATE, LOCATION, 
 New_Deaths_per_Million AS New_Deaths_per_Million, Total_Deaths_per_Million AS Total_Deaths_per_Million
 FROM {{source("covid19_data", "ALL_DATA")}}
